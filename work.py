4cb74d78169db422e8942d33eaa9de0d54ddc70f
blueeejay <jadec153@nycstudents.net>
2026-02-02 10:16:34 -0500

    Create work.py

---
+++ work.py
@@ -0,0 +1,28 @@
+# integers and floats: tip calculator
+""" 
+values = [1,2.23,5,7,2,30,15] """
+""" print(values)
+for i in values:
+    print(i) """
+""" 
+print(values[0])
+print(values[6]) """
+
+""" x = "this is a thing"
+y = x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+
+""" challenge: Using the "input" method in Python, 
+ask a user to input a sentence. 
+ develop a function that accepts a the user input 
+ will tell you how many words are in that string.
+ First write out your plan in Pseudo-code using comments.
+ Then craft the function. """
+
+sentence = input(" Write a sentence: ") 
+
+def wordcount:
+   print(x)
